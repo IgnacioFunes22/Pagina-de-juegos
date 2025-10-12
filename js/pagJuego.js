ccf00c25111642ead5f184d4aa53453b361ac324
Ignacio Funes <nachofunes22@gmail.com>
2025-10-12 20:41:26 -0300

    pagJuegos vr 2.0 implementando js

---
+++ js/pagJuego.js
@@ -0,0 +1,26 @@
+var mainSlider = document.getElementById("imagen");
+var selectorSlider = document.getElementsByClassName("visual");
+
+selectorSlider[0].onclick = function(){
+    mainSlider .src = selectorSlider[0].src;
+}
+
+selectorSlider[1].onclick = function(){
+    mainSlider .src = selectorSlider[1].src;
+}
+
+selectorSlider[2].onclick = function(){
+    mainSlider .src = selectorSlider[2].src;
+}
+
+selectorSlider[3].onclick = function(){
+    mainSlider .src = selectorSlider[3].src;
+}
+
+selectorSlider[4].onclick = function(){
+    mainSlider .src = selectorSlider[4].src;
+}
+
+selectorSlider[5].onclick = function(){
+    mainSlider .src = selectorSlider[5].src;
+}
